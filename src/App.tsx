bcef2f2184284399466b0d6d4790e1e2b37c06fd
OriAfiasDev <ori@afias.net>
2021-11-30 20:31:12 +0200

    cleanup

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,10 @@
-import React, { useReducer } from 'react';
-import logo from './logo.svg';
-import './App.css';
-import { useDispatch, useSelector } from 'react-redux';
-import { themeSelector } from './redux/selectors';
-import { toggleTheme } from './redux/actions/theme';
+import { Header } from './components/Header';
 
 function App() {
-	const theme = useSelector(themeSelector);
-	const dispatch = useDispatch();
 	return (
-		<div className='App'>
-			<header className='App-header'>
-				<img src={logo} className='App-logo' alt='logo' />
-				<p onClick={() => dispatch(toggleTheme(theme))}>
-					Edit <code>src/App.tsx</code> and save to reload.
-				</p>
-				<a className='App-link' href='https://reactjs.org' target='_blank' rel='noopener noreferrer'>
-					{theme}
-				</a>
-			</header>
-		</div>
+		<>
+			<Header />
+		</>
 	);
 }
 
--- src/index.tsx
+++ src/index.tsx
@@ -2,25 +2,19 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
-
-import { Provider as ReduxProvider } from 'react-redux';
-import { createStore } from 'redux';
-import reducers from './redux';
-import { composeWithDevTools } from 'redux-devtools-extension';
-
-const store = createStore(reducers, composeWithDevTools());
+import { ReduxProvider } from './Providers/ReduxProvider';
+import { ThemeProvider } from './Providers/MuiTheme';
+import { BrowserRouter as Router } from 'react-router-dom';
 
 ReactDOM.render(
-	// <React.StrictMode>
-	<ReduxProvider store={store}>
-		<App />
-	</ReduxProvider>,
-	// </React.StrictMode>,
+	<React.StrictMode>
+		<ReduxProvider>
+			<ThemeProvider>
+				<Router>
+					<App />
+				</Router>
+			</ThemeProvider>
+		</ReduxProvider>
+	</React.StrictMode>,
 	document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
